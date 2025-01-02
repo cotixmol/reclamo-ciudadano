3eca46e4862dc90273ce8fece34419398be632df
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-02 15:38:34 -0300

    eslint/ formatter changes on first files

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,8 @@ import uvicorn
 
 app = FastAPI(root_path="/api/v1")
 
-app.include_router(router) 
+app.include_router(router)
+
 
 if __name__ == "__main__":
-    uvicorn.run(router, host="0.0.0.0", port=8000) 
+    uvicorn.run(router, host="0.0.0.0", port=8000)
--- backend/routers/reports.py
+++ backend/routers/reports.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/reports/") 
+
+@router.get("/reports/")
 async def read_reports():
-    return [{"report_id": 1, "report_name": "Report 1"}, {"report_id": 2, "report_name": "Report 2"}]
+    return [
+        {"report_id": 1, "report_name": "Report 1"},
+        {"report_id": 2, "report_name": "Report 2"},
+    ]
