3cd139f7ee71e19a81acaa9569ae07024d17e592
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-06 18:47:23 -0300

    feat/ Creation of custom errors

---
+++ backend/errors/__init__.py
@@ -0,0 +1,1 @@
+from .claim_errors import ClaimsNotFound
---
+++ backend/errors/claim_errors.py
@@ -0,0 +1,23 @@
+class ClaimsNotFound(Exception):
+    """
+    Raised when no claims are found in the database.
+    """
+
+    def __init__(
+        self,
+        message=None,
+    ):
+        if message is None:
+            message = "No claims found in the database. Please check your search criteria or ensure that claims have been submitted."
+        super().__init__(message)
+
+
+class ClaimNotFound(Exception):
+    """
+    Raised when a specific claim is not found.
+    """
+
+    def __init__(self, claim_id, message=None):
+        if message is None:
+            message = f"Claim with id {claim_id} not found in the database. Please verify the claim ID and try again."
+        super().__init__(message)
