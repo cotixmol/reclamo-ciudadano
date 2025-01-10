7aab16085a3255656e6ae9e903a0702382a955ff
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-10 16:28:15 -0300

    config / next.js configuration of main services

--- frontend-rc/tailwind.config.ts
+++ frontend-rc/tailwind.config.ts
@@ -1,18 +1,12 @@
-import type { Config } from "tailwindcss";
-
-export default {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", 
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}", 
+    "./src/**/*.{html,js,jsx,ts,tsx}", 
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
