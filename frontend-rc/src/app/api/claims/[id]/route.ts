5aa66935d6c0a16095bb3da6e51a1e758257822d
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-10 18:01:07 -0300

    feat/ import claim change

--- frontend-rc/src/app/api/claims/[id]/route.ts
+++ frontend-rc/src/app/api/claims/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
-import { Claim } from "@/app/utils/types";
+import { Claim  } from "@/app/models/claims/utils/types";
 
 export async function GET(
   request: Request,
