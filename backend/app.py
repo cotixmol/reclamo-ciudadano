a680858b0ab5a6cbfbb9f6f64e974e78d224337a
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-10 16:27:53 -0300

    feat/ configure cors middleware in fastAPI backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routers.claim import router
 import uvicorn
+from middleware.cors import add_cors_middleware
 
 app = FastAPI(root_path="/api/v1")
 
-app.include_router(router)
+add_cors_middleware(app)
 
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
