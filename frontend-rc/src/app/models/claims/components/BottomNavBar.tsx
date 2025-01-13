71123ed03167074609c18dac09c4712ef58183ed
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-13 12:35:21 -0300

    feat/ bottom nav bar improvement

--- frontend-rc/src/app/models/claims/components/BottomNavBar.tsx
+++ frontend-rc/src/app/models/claims/components/BottomNavBar.tsx
@@ -1,8 +1,8 @@
-// components/BottomNavBar.tsx
 "use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FiHome, FiPlus, FiList } from "react-icons/fi";
 
 const BottomNavBar = () => {
   const pathname = usePathname();
@@ -10,72 +10,54 @@ const BottomNavBar = () => {
   const isActive = (path: string) => pathname === path;
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-gray-900 text-gray-200 shadow-lg">
-      <div className="flex items-center justify-between px-6 py-4">
+    <nav className="fixed bottom-0 left-0 w-full bg-gray-900 text-gray-200 shadow-lg shadow-black/30 border-t border-[#e4047d]">
+      <div className="flex items-center justify-between px-10 py-6">
         {/* Home Button */}
-        <Link href="/" className="flex flex-col items-center space-y-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className={`w-6 h-6 ${
-              isActive("/") ? "text-[#e4047d]" : "text-gray-400"
+        <Link
+          href="/"
+          className={`flex flex-col items-center space-y-1 ${
+            isActive("/") ? "scale-125" : "scale-100"
+          } transition-transform duration-200`}
+        >
+          <FiHome
+            className={`w-8 h-8 ${
+              isActive("/") ? "text-[#e4047d] shadow-pink-500" : "text-gray-400"
             }`}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 12l9-9m0 0l9 9m-9-9v18"
-            />
-          </svg>
-          <span className="text-sm">Home</span>
+          />
+          <span className={`text-sm ${isActive("/") ? "text-[#e4047d]" : ""}`}>
+            Home
+          </span>
         </Link>
 
         {/* Create New Claim Button */}
         <Link
           href="/create"
-          className="relative z-10 bg-[#e4047d] text-white rounded-full p-4 shadow-lg hover:bg-[#ff4da6] transition duration-200"
+          className="relative z-10 bg-[#e4047d] text-white rounded-full p-5"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
+          <FiPlus className="w-8 h-8" />
         </Link>
 
         {/* My Claims Button */}
         <Link
-          href="/my-claims"
-          className="flex flex-col items-center space-y-1"
+          href="/models/claims/pages"
+          className={`flex flex-col items-center space-y-1 ${
+            isActive("/models/claims/pages") ? "scale-125" : "scale-100"
+          } transition-transform duration-200`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className={`w-6 h-6 ${
-              isActive("/my-claims") ? "text-[#e4047d]" : "text-gray-400"
+          <FiList
+            className={`w-8 h-8 ${
+              isActive("/models/claims/pages")
+                ? "text-[#e4047d] shadow-pink-500"
+                : "text-gray-400"
+            }`}
+          />
+          <span
+            className={`text-sm ${
+              isActive("/models/claims/pages") ? "text-[#e4047d]" : ""
             }`}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.5 20.25v-4.5m-9 4.5v-9m13.5 9V12.75m-18 7.5V8.25m13.5 12h-9"
-            />
-          </svg>
-          <span className="text-sm">My Claims</span>
+            My Claims
+          </span>
         </Link>
       </div>
     </nav>
