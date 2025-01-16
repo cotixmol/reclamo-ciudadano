a80da63a490d2d914134ea03a201cad776b9c490
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-16 15:33:15 -0300

    directory / move utils folder

--- frontend-rc/src/app/api/claims/create/route.ts
+++ frontend-rc/src/app/api/claims/create/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 import axios from 'axios';
-import { toCamelCase } from '@/app/utils/toCamelCase';
+import { toCamelCase } from '@/utils/toCamelCase';
 import {
   ClaimCreateRequest,
   ClaimResponse,
