31af5180ac32057d173f7b23a1687213012b89c6
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-20 16:43:07 -0300

    feat / improvement in styles in map

--- frontend-rc/src/app/models/claims/components/ClaimForm.tsx
+++ frontend-rc/src/app/models/claims/components/ClaimForm.tsx
@@ -128,10 +128,8 @@ export default function ClaimForm() {
           <div className="w-full rounded overflow-hidden ">
             {/* New heading and subtitle */}
             <div className="mb-4">
-              <h3 className="text-lg font-semibold">
-                {t('selectLocationTitle')}
-              </h3>
-              <p className="text-sm text-gray-400">
+              <h3 className="block mb-1">{t('selectLocationTitle')}</h3>
+              <p className="text-sm text-gray-500 mt-1">
                 {t('selectLocationSubtitle')}
               </p>
             </div>
