712cbd571107bd2703ab43bd57dc0041398ce8e1
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-21 10:08:25 -0300

    fix / update of cors policy

--- backend/middleware/cors.py
+++ backend/middleware/cors.py
@@ -5,13 +5,17 @@ def add_cors_middleware(app):
     """Add CORS middleware to the FastAPI app."""
 
     origins = [
-        "http://localhost:3000",  # Replace with your Next.js frontend's origin
+        "http://localhost:3000",
+        "http://dev.reclamo-ciudadano.reputacion.digital:3007",
+        "https://dev.reclamo-ciudadano.reputacion.digital",
+        "http://190.2.8.138:3007",
+        "https://190.2.8.138",
     ]
 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_credentials=True,  # If you need to send cookies, set this to True
-        allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
-        allow_headers=["*"],  # Allow all headers
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
