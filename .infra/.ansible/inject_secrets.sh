7e2d4b88d2385531ea18a65d3a474c398d2586d9
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-22 11:53:15 -0300

    infra / update inject_secrets to insert NEXT_PUBLIC_VERSION

--- .infra/.ansible/inject_secrets.sh
+++ .infra/.ansible/inject_secrets.sh
@@ -3,7 +3,7 @@
 # Define the target file
 TARGET_FILE=".env"
 
-# Loop through all GitLab CI variables starting with "SECREET_"
+# Loop through all GitLab CI variables starting with "SECRET_"
 for var in $(compgen -e); do
     if [[ $var == SECRET_* ]]; then
         if grep -q "$var" $TARGET_FILE; then
@@ -16,4 +16,19 @@ done
 
 awk '{for(i=1;i<=NF;i++) if ($i ~ /^SECRET_/) print $i}' $TARGET_FILE
 
-echo "Replacements completed in $TARGET_FILE."
+# Append or update NEXT_PUBLIC_VERSION in .env
+if [ -f VERSION ]; then
+    echo "Adding/updating version info from VERSION file to $TARGET_FILE."
+    version_line=$(cat VERSION)
+    public_version_line="NEXT_PUBLIC_${version_line}"
+    
+    if grep -q '^NEXT_PUBLIC_VERSION=' "$TARGET_FILE"; then
+        sed -i "s|^NEXT_PUBLIC_VERSION=.*|$public_version_line|" "$TARGET_FILE"
+        echo "Updated NEXT_PUBLIC_VERSION in $TARGET_FILE."
+    else
+        echo "$public_version_line" >> "$TARGET_FILE"
+        echo "Appended NEXT_PUBLIC_VERSION to $TARGET_FILE."
+    fi
+fi
+
+echo "Replacements and version update completed in $TARGET_FILE."
