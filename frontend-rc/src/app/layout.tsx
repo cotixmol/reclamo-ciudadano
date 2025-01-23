50424daeacb67d5a5352170d275dc49a9bc17936
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-23 17:26:47 -0300

    feat / pwa manifest with icons and considerations for several devices

--- frontend-rc/src/app/layout.tsx
+++ frontend-rc/src/app/layout.tsx
@@ -23,8 +23,21 @@ export const metadata: Metadata = {
   title: 'Reclamo Ciudadano',
   description:
     'Reporta problemas en tu ciudad y sigue el estado de los reclamos.',
+  manifest: '/manifest.json',
+  themeColor: '#e4047d',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Reclamo Ciudadano',
+  },
+
   icons: {
-    icon: '/favicon.png',
+    icon: [
+      { url: '/favicon.png' },
+      { url: '/icon-192.png', sizes: '192x192' },
+      { url: '/icon-512.png', sizes: '512x512' },
+    ],
+    apple: [{ url: '/icon-180.png', sizes: '180x180' }],
   },
 };
 
@@ -39,8 +52,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className="relative min-h-screen bg-gray-900 text-gray-200">
         <ClaimTypesProvider claimTypes={claimTypes}>
-          <div className="pb-28">{children}</div>{' '}
-          {/* Add padding for nav bar */}
+          <div className="pb-28">{children}</div>
           <BottomNavBar />
         </ClaimTypesProvider>
       </body>
