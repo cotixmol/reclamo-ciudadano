89bfd61001f8093316780b0b9300a3cd5391599e
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-24 13:55:40 -0300

    next config / improvement

--- frontend-rc/next.config.ts
+++ frontend-rc/next.config.ts
@@ -1,42 +1,40 @@
 import type { NextConfig } from 'next';
 
-/**
-1) Define your base Next.js config here (any standard Next.js settings).
- */
+// Start with a base Next.js config
+// 1) Add the `images` config to allow images from any HTTPS domain
+// 2) Add the `env` config to expose the API_URL environment variable
 const nextConfig: NextConfig = {
-  // Remote image patterns
+  // Images from any HTTPS domain are allowed
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*', // Allow images from any HTTPS domain
+        hostname: '*',
       },
     ],
   },
 
   env: {
-    API_URL: process.env.API_URL || 'http://localhost:8000/api/v1',
+    API_URL: process.env.API_URL
   },
 };
 
-/**
- * 2) Require (or import) `next-pwa` and call it with ONLY your PWA-specific options:
- *    - `dest`: Where to output the service worker files
- *    - `register`: Automatically registers the service worker in the client
- *    - `skipWaiting`: Immediately activate the new service worker on update
- */
-const withPWA = require('next-pwa')({
+
+// 2) Require `next-pwa` library and call it with ONLY your PWA-specific options:
+//    - `dest`: Where to output the service worker files
+//    - `register`: Automatically registers the service worker in the client
+//    - `skipWaiting`: Immediately activate the new service worker on update
+const nextPWACustom = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
 });
 
-/**
- * 3) Export the merged config by calling `withPWA(...)` with:
- *    - Your base Next.js config (`...nextConfig`)
- *    - Any additional top-level Next.js options you'd like to override
- */
-module.exports = withPWA({
+
+// 3) Export the merged config by calling `nextPWACustom(...)` with:
+//   - Your base Next.js config (`...nextConfig`)
+//   - Any additional top-level Next.js options you'd like to override
+module.exports = nextPWACustom({
   ...nextConfig,
-  reactStrictMode: true, // Enforce React Strict Mode
+  reactStrictMode: true,
 });
