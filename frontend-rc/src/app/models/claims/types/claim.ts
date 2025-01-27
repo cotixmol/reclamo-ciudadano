1bbaa957733de6ade010bccf25d87ce2da8ae2ad
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-01-27 12:31:26 -0300

    feat / claim update body

--- frontend-rc/src/app/models/claims/types/claim.ts
+++ frontend-rc/src/app/models/claims/types/claim.ts
@@ -38,6 +38,7 @@ export interface ClaimUpdateRequest {
   description?: string;
   status?: 'Open' | 'Close' | string;
   claim_location?: ClaimLocation;
+  priority?: PriorityEnum;
 }
 
 /** API Response Interfaces **/
