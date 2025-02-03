a747185acd26f284bee21f41f1929158574a05aa
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-02-03 18:43:01 -0300

    fix/ test deploy test

--- frontend-rc/src/app/services/s3/putObject.ts
+++ frontend-rc/src/app/services/s3/putObject.ts
@@ -13,11 +13,11 @@ export const PutObjectInS3 = async (url: string, file: File): Promise<void> => {
     const response = await axios.put(url, file, config);
     
 
-    if (response.status !== 200 && response.status !== 204) {
-      throw new Error(
-        `Failed to upload ${file.name} to S3. Status Code: ${response.status}`
-      );
-    }
+    // if (response.status !== 200 && response.status !== 204) {
+    //   throw new Error(
+    //     `Failed to upload ${file.name} to S3. Status Code: ${response.status}.`
+    //   );
+    // }
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
       const statusText = error.response?.statusText || 'Unknown error';
