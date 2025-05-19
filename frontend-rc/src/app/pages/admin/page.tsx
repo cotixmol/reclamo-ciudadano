c29607d276416eade0d30af8e74a33565415fdeb
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-05-19 14:49:28 -0300

    feat/ admin page file creation.

---
+++ frontend-rc/src/app/pages/admin/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { ClaimWithMultimediaResponse } from '../../models/claims/types/claim';
+import ClaimCard from '../../models/claims/components/ClaimCard';
+import LoadingScreen from '@/app/components/LoadingScreen';
+import ErrorPage from '@/app/components/ErrorPage';
+import ClaimNotFoundPage from '../../models/claims/components/ClaimNotFound';
+import { fetchAllClaimsByPublicIds } from '@/app/services/claims/fetch';
+
+export default function AdminPage() {
+  const [claims, setClaims] = useState<ClaimWithMultimediaResponse[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const loadClaims = async () => {
+      setIsLoading(true);
+      try {
+        const claimsData = await fetchAllClaimsByPublicIds();
+        setClaims(claimsData.reverse());
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadClaims();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (error) {
+    return <ErrorPage message={error.message} />;
+  }
+
+  if (!claims || claims.length === 0) {
+    return <ClaimNotFoundPage />;
+  }
+
+  return (
+    <div className="p-5 flex justify-center items-start">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {claims.map((claimData) => (
+          <div></div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- frontend-rc/src/app/services/claims/fetch.ts
+++ frontend-rc/src/app/services/claims/fetch.ts
@@ -11,7 +11,6 @@ export async function fetchAllClaimsByPublicIds(): Promise<
 > {
   const publicIds = getStoredPublicIds();
   try {
-    console.log(`Requesting claims to ${process.env.NEXT_PUBLIC_INTERNAL_API_URL}`)
     const response = await axios.post<ClaimWithMultimediaResponse[]>(
       `${process.env.NEXT_PUBLIC_INTERNAL_API_URL}/claims`,
       {
@@ -49,3 +48,21 @@ export async function fetchClaimByPublicId(
     throw new Error('An unknown error occurred while fetching claim');
   }
 }
+
+export async function fetchClaimByAdmin(
+  publicId: string
+): Promise<ClaimWithMultimediaResponse> {
+  try {
+    const response = await axios.get<ClaimWithMultimediaResponse>(
+      `${process.env.NEXT_PUBLIC_INTERNAL_API_URL}/claims/${publicId}`
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const { data } = error.response;
+      throw new Error(data?.error || 'Failed to fetch claim');
+    }
+    throw new Error('An unknown error occurred while fetching claim');
+  }
+}
+
